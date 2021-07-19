8e076ae09fbad522a65d18db26256a6c4ef167dc
Shreya Prakash <66696495+shreyaa021@users.noreply.github.com>
2021-07-19 14:45:54 +0530

    Update colorManual.cs
    Dependency Injection

--- TelCo.ColorCoder/colorManual.cs
+++ TelCo.ColorCoder/colorManual.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Drawing;
@@ -6,19 +6,54 @@ using System.Text;
 
 namespace TelCo.ColorCoder
 {
-    class colorManual
+    class printManual:IReferenceManual
     {
-        
-        public static void manual()
+        private int max_length = colorFromPairnum.colorMapMajor.Length;
+        private int min_length = colorFromPairnum.colorMapMinor.Length;
+        public  void manual()
         {
+            Console.WriteLine(" Printing color manual \n**********\n");
 
-           int pno = 1;
-            for (int i = 0; i < 5; i++)
+
+          int pno = 1;
+            for (int i = 0; i < max_length; i++)
             {
-                for (int j = 0; j < 5; j++)
+                for (int j = 0; j < min_length; j++)
                 {
 
                     Console.WriteLine("[In]Pair Number: {0},Colors: major{1} - minor{2}\n", pno, colorFromPairnum.colorMapMajor[i], colorFromPairnum.colorMapMinor[j]);
+                    pno++;
+                }
+            }
+
+        }
+
+    }
+    class testmanual:IReferenceManual
+    {
+        private int max_length = colorFromPairnum.colorMapMajor.Length;
+        private int min_length = colorFromPairnum.colorMapMinor.Length;
+        public  void manual()
+        {
+            Console.WriteLine("Testing reference manual/n **********/n");
+
+             int pno = 1;
+            for (int i = 0; i < max_length; i++)
+            {
+                for (int j = 0; j < min_length; j++)
+                {
+
+                    ColorPair testPair = colorFromPairnum.GetColorFromPairNumber(pno);
+                    Debug.Assert(testPair.majorColor == colorFromPairnum.colorMapMajor[i]);
+                    Debug.Assert(testPair.minorColor == colorFromPairnum.colorMapMinor[j]);
+
+                   var test = new ColorPair();
+
+                    test.majorColor = colorFromPairnum.colorMapMajor[i];
+                    test.minorColor = colorFromPairnum.colorMapMinor[j];
+                   
+                    int pairNumber = ColorPair.GetPairNumberFromColor(testPair);
+                    Debug.Assert(pno == pairNumber);
                     pno++;
                 }
             }
